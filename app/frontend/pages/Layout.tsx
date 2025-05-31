41b905d4f48dd211ae0e494cff95a5f2d07ee0b5
Varun <rvarunr5@gmail.com>
2025-05-31 07:03:02 -0400

    removed mantine

--- app/frontend/pages/Layout.tsx
+++ app/frontend/pages/Layout.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { MantineProvider } from "@mantine/core";
-import { Notifications } from "@mantine/notifications";
-import { theme } from "../lib/mantine";
-import "@mantine/core/styles.css";
-import "@mantine/notifications/styles.css";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return (
-    <MantineProvider theme={theme}>
-      <Notifications />
-      {children}
-    </MantineProvider>
-  );
+  return <>{children}</>;
 }
